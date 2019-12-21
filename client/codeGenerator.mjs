cb89ce6897e451ec101c66a1c2b58e7f5d87afe9
Ивановский Дмитрий Александрович <d.ivanovskii@kontur>
2019-12-21 16:33:39 +0500

    adds code gener

---
+++ client/codeGenerator.mjs
@@ -0,0 +1,4 @@
+const urlParams = new URLSearchParams(window.location.search);
+const code = urlParams.get('code');
+console.log(code);
+document.querySelector('#code').textContent = 'Код вашей лекции: ' + code;
