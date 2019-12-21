a5e1ab6a84380b88aba5559ef13aac5f6c81f4c9
slysheva <alexandra.slysheva@yandex.ru>
2019-12-21 16:11:20 +0500

    add form submition

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const ws = require('ws');
 const path = require('path');
-
+const bodyParser = require("body-parser");
 const port = 3000;
 const emogies = [];
 const statistics = [];
+const multer  = require('multer');
+const upload = multer();
 
 const event_keys = new Set(['123']);
 
@@ -16,6 +18,17 @@ app.use(express.static(path.join(process.cwd(), 'client')));
 const server = app.listen(port);
 
 const wss = new ws.Server({noServer: true});
+
+app.use(bodyParser.json());
+
+
+app.post("/formPage.html", upload.none(), (req, res) => {
+    let result = req.body;
+    console.log(result);
+
+
+    res.redirect('./codePage.html?code=12345');
+});
 
 
 server.on('upgrade', (req, socket, head) => {
@@ -38,3 +51,4 @@ server.on('upgrade', (req, socket, head) => {
         );
     }
 });
+
