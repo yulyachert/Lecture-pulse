f4e5662d9e4a4d07efe0bbefbace41f69e5cc0ee
Ивановский Дмитрий Александрович <d.ivanovskii@kontur>
2019-12-21 13:13:58 +0500

    server start

--- server.js
+++ server.js
@@ -1,55 +1,27 @@
 const express = require('express');
+const ws = require('ws');
+const path = require('path');
+
+const port = 3000;
+const emogies = [];
+const statistics = [];
+const event_keys = new Set();
+const connections = [];
 
 const app = express();
-const port = 3000;
+app.use();
+app.use(express.static(path.join(process.cwd(), 'client')));
+
+const server = app.listen(port);
+
+const wss = new WebSocket.Server({noServer: true});
 
 
-app.use(express.static('static'));
+server.on('upgrade', (req, socket, head) => {
+    const url = new URL(req.url, req.headers.origin);
+    console.log(url);
+    const eventKey = url.searchParams.get('eventKey');
+    //Проверка
+    console.log(eventKey);
 
-app.listen(port, (err) => {
-    if (err) {
-        return console.log('something bad happened', err);
-    }
-    console.log(`server is listening on ${port}`);
-});
-
-animal_questions = [
-    {
-        'question': 'Какой тигр самый крупный?',
-        'variants': ["Амурский", 'Малазийский', 'Индийский', 'Сумматранский'],
-        'answer': 'Амурский',
-        'picture': '/static/tiger.jpg'
-    },
-    {
-        'question': 'Что говорит кошечка?',
-        'variants': ["Мяу", 'Гав', 'Кукареку', 'Хрю'],
-        'answer': 'Мяу',
-        'picture': '/static/cat.webp'
-    },
-    {
-        'question': 'Что говорит собачка?',
-        'variants': ["Мяу", 'Гав', 'Кукареку', 'Хрю'],
-        'answer': 'Гав',
-        'picture': '/static/dog.webp'
-    }
-];
-
-app.get('/quiz/animals', (request, response) => {
-    const to_send = {'items': []};
-    for (const item of animal_questions) {
-        to_send['items'].push({
-            'question': item['question'],
-            'variants': item['variants'],
-            'picture': item['picture']
-        })
-    }
-    response.json(to_send);
-});
-
-app.post('/quiz/animals', (request, response) => {
-    console.log(request);
-});
-
-app.get('/orgsList', (request, response) => {
-    response.json(ORGS_LIST);
 });
